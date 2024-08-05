1e9b0e689b9b283f898b44fb541b16c7242e205b
Bao_dev <>
2024-08-05 16:38:46 +0700

    Updates

---
+++ precache-manifest.609f588d7e4738f61e85745e167008b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388b83e79532d54e9482f13d764d1cc6",
+    "url": "/Patient-Management-Application/index.html"
+  },
+  {
+    "revision": "7bcbf30a7bb3e51cb16e",
+    "url": "/Patient-Management-Application/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "6b03b43c2dace2bf0415",
+    "url": "/Patient-Management-Application/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7bcbf30a7bb3e51cb16e",
+    "url": "/Patient-Management-Application/static/js/2.cec931c6.chunk.js"
+  },
+  {
+    "revision": "f81b1d521955ed7a361461485de1c1c0",
+    "url": "/Patient-Management-Application/static/js/2.cec931c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "644892a8954fa13a8ba4",
+    "url": "/Patient-Management-Application/static/js/3.8d5dac00.chunk.js"
+  },
+  {
+    "revision": "6b03b43c2dace2bf0415",
+    "url": "/Patient-Management-Application/static/js/main.9988fd7c.chunk.js"
+  },
+  {
+    "revision": "1861ba01e22daf46282c",
+    "url": "/Patient-Management-Application/static/js/runtime-main.f3e32b1b.js"
+  }
+]);
